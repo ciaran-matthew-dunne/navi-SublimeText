bc0101873d1337f9323141a88eb69f948dff2384
ciaran <cmd1@hw.ac.uk>
2023-05-22 19:18:11 +0100

    updating

--- navi_plugin.py
+++ navi_plugin.py
@@ -2,11 +2,6 @@ import os, sys
 import requests, json
 import sublime, sublime_plugin
 import re
-
-# Okay. Our main Navi python program needs to be running in Python 3.10 to access OpenAI and tinyGPT.
-# The plugin will just communicate between ST4 and Navi.
-# (e.g., bundles of code sent to ST4 for execution, state passed to Navi-GPT REPL, stdout in repl.) 
-
 def navi_to_sublime(navi_command):
     # Fetch active window and view
     window = sublime.active_window()
@@ -14,78 +9,45 @@ def navi_to_sublime(navi_command):
     edit = view.begin_edit()
 
     # Parse the Navi command and extract the action and arguments
-    action_args = navi_command.split(' ')
-    action = action_args[0]
-    args = action_args[1:]
+    action, *args = navi_command.split(' ')
 
-    # Match action to corresponding Sublime Text command
-    if action == 'focus-group':
-        window.focus_group(int(args[0]))
-    elif action == 'focus-view':
-        view = window.views_in_group(window.active_group())[int(args[0])]
-        window.focus_view(view)
-    elif action == 'view-open':
-        window.open_file(args[0])
-    elif action == 'view-close':
-        window.run_command('close')
-    elif action == 'insert':
-        view.insert(edit, view.sel()[0].begin(), ' '.join(args))
-    elif action == 'insert-line':
-        view.insert(edit, view.sel()[0].begin(), ' '.join(args) + '\n')
-    elif action == 'delete':
-        region_to_erase = sublime.Region(view.sel()[0].begin(), view.sel()[0].begin() + int(args[0]))
-        view.erase(edit, region_to_erase)
-    elif action == 'delete-line':
-        line_region = view.line(view.sel()[0])
-        view.erase(edit, line_region)
-    elif action == 'select-all':
-        view.sel().clear()
-        view.sel().add(sublime.Region(0, view.size()))
-    elif action == 'select-word':
-        word_region = view.word(view.sel()[0])
-        view.sel().clear()
-        view.sel().add(word_region)
-    elif action == 'select-line':
-        line_region = view.line(view.sel()[0])
-        view.sel().clear()
-        view.sel().add(line_region)
-    elif action == 'move-to':
-        point = view.text_point(int(args[0]), int(args[1]))
-        view.sel().clear()
-        view.sel().add(sublime.Region(point))
-    elif action == 'find':
-        regions = view.find_all(args[0])
-        if regions:
-            view.sel().clear()
-            view.sel().add(regions[0])
-    elif action == 'copy':
-        sublime.set_clipboard(view.substr(view.sel()[0]))
-    elif action == 'paste':
-        view.insert(edit, view.sel()[0].begin(), sublime.get_clipboard())
-    elif action == 'undo':
-        view.run_command('undo')
-    elif action == 'redo':
-        view.run_command('redo')
-    elif action == 'replace':
-        view.run_command('replace_all', {"find": args[0], "replace": args[1]})
-    elif action == 'save':
-        view.run_command('save')
-    elif action == 'rename':
-        os.rename(args[0], args[1])
-    elif action == 'create-dir':
-        os.makedirs(args[0], exist_ok=True)
-    elif action == 'delete-file':
-        os.remove(args[0])
-    elif action == 'comment-line':
-        view.run_command('toggle_comment', {"block": False})
-    elif action == 'uncomment-line':
-        view.run_command('toggle_comment', {"block": False})
-    elif action == 'indent':
-        view.run_command('indent')
-    elif action == 'dedent':
-        view.run_command('dedent')
+    # Define action mapping
+    actions = {
+        'focus-group': lambda: window.focus_group(int(args[0])),
+        'focus-view': lambda: window.focus_view(window.views_in_group(window.active_group())[int(args[0])]),
+        'view-open': lambda: window.open_file(args[0]),
+        'view-close': lambda: window.run_command('close'),
+        'insert': lambda: view.insert(edit, view.sel()[0].begin(), ' '.join(args)),
+        'insert-line': lambda: view.insert(edit, view.sel()[0].begin(), ' '.join(args) + '\n'),
+        'delete': lambda: view.erase(edit, sublime.Region(view.sel()[0].begin(), view.sel()[0].begin() + int(args[0]))),
+        'delete-line': lambda: view.erase(edit, view.line(view.sel()[0])),
+        'select-all': lambda: view.sel().add(sublime.Region(0, view.size())),
+        'select-word': lambda: view.sel().add(view.word(view.sel()[0])),
+        'select-line': lambda: view.sel().add(view.line(view.sel()[0])),
+        'move-to': lambda: view.sel().add(sublime.Region(view.text_point(int(args[0]), int(args[1])))),
+        'find': lambda: view.sel().add(view.find_all(args[0])[0]) if view.find_all(args[0]) else None,
+        'copy': lambda: sublime.set_clipboard(view.substr(view.sel()[0])),
+        'paste': lambda: view.insert(edit, view.sel()[0].begin(), sublime.get_clipboard()),
+        'undo': lambda: view.run_command('undo'),
+        'redo': lambda: view.run_command('redo'),
+        'replace': lambda: view.run_command('replace_all', {"find": args[0], "replace": args[1]}),
+        'save': lambda: view.run_command('save'),
+        'rename': lambda: os.rename(args[0], args[1]),
+        'create-dir': lambda: os.makedirs(args[0], exist_ok=True),
+        'delete-file': lambda: os.remove(args[0]),
+        'comment-line': lambda: view.run_command('toggle_comment', {"block": False}),
+        'uncomment-line': lambda: view.run_command('toggle_comment', {"block": False}),
+        'indent': lambda: view.run_command('indent'),
+        'dedent': lambda: view.run_command('dedent')
+    }
 
-    view.end_edit(edit)  
+    # Call the action
+    if action in actions:
+        actions[action]()
+    else:
+        print("Action {action} not found".format())
+
+    view.end_edit(edit)
 
 def process_navi_script(navi_script):
   # Split the script into separate commands and execute each one
@@ -152,18 +114,19 @@ def sublime_state():
       state_info += "{} {}\n".format(i, view)
   return state_info
 
-# Command for sending prompt to Navi server 
+# Command for sending prompt to Navi server
 class NaviCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        sublime.active_window().show_input_panel("", "", self.on_done, None, None)
 
-  def run(self, edit):
-    sublime.active_window().show_input_panel("", "", self.on_done, None, None)
+    def on_done(self, user_input):
+      url = "http://localhost:5000/receive"
+      headers = {"Content-Type": "application/json"}
+      subl_data = json.dumps(
+        { "user_prompt": user_input, "subl_state": sublime_state() }
+      )
 
-  def on_done(self, user_input):
-    url = "http://localhost:5000/receive"
-    headers = {"Content-Type": "application/json"}
-    subl_data = json.dumps(
-      {"user_prompt" : user_input, 
-       "subl_state" : sublime_state()
-      })
-    response = requests.post(url, headers=headers, data=subl_data)
-    
+      response = requests.post(url, headers=headers, data=subl_data, stream=True)
+      print(response)
+      for chunk in response.iter_content(chunk_size=100*8192, decode_unicode=True):
+        print(chunk)
